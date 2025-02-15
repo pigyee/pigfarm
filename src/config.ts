80375e4b53041340a29bb7320b2f3295789ff047
pig810 <136228019+pigyee@users.noreply.github.com>
2025-02-15 19:46:49 +0800

    update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -40,52 +40,16 @@ export const CONTACTS = {
 
 export const SLOGAN = "由pig810运营";
 export const ME_DESCRIPTION =
-  `Hi, I’m Leo! Since childhood, I’ve dreamed of becoming an astronaut, which shaped my passion for exploration and curiosity about the unknown. I’m a creative thinker with strong logical reasoning skills, allowing me to approach challenges innovatively yet systematically. I thrive on discovering new ideas and pushing boundaries, whether in my work or personal growth. My motto, “Explore endlessly, surpass yourself,” reflects my dedication to learning and achieving. Combining creativity with analytical thinking, I’m eager to contribute to meaningful projects and explore new opportunities!`;
-
-
-export const WORK_EXPERIENCE = [
-  {
-    company: "Galactic Ventures",
-    position: "Innovation Explorer",
-    startDate: "2022",
-    endDate: "2024",
-  },
-  
-  {
-    company: "Interstellar Academy Program",
-    position: "Astro Trainee",
-    startDate: "2023",
-    endDate: "2023",
-  },
-  
-  {
-    company: "LunarTech Solutions",
-    position: "Creative Logic Engineer",
-    startDate: "2020",
-    endDate: "2022",
-  },
-  
-  {
-    company: "Cosmic Coders Inc.",
-    position: "Frontend Navigator",
-    startDate: "2018",
-    endDate: "2020",
-  },
-  
-];
-
+  `我是pig810，初中生，热爱科技。喜欢折腾电子垃圾，写网页，是个半吊子前端。梦想发明有用的小科技，爱看科幻电影。`;
 
 export const PROJECTS = [
     {
-      id: "my-first-rocket-launch",
-      title: "My First Rocket Launch",
-      description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
-      image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      externalLink: "https://www.nasa.gov/",
+      id: "我的网站",
+      title: "我的网站",
+      description: "",
+      image: '',
+      externalLink: "https://blog.nqh.us.kg",
       details: [
-        "Explaining space exploration concepts through creative illustrations.",
-        "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
-        "Won an award in the interstellar creativity competition.",
       ],
     },
   ];
